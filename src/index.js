d60746df4eacc6e4a36e468b2a5662e9ca4e6a23
crsnbogdan <crsnbogdan@gmail.com>
2022-04-28 00:22:15 +0300

    initial project commit

---
+++ src/index.js
@@ -0,0 +1,209 @@
+function ShipFactory(shipLength) {
+    let ship = [];
+    for (let i = 0; i < shipLength; i++) {
+        ship.push([])
+    }
+
+    ship.forEach(shipPiece => {
+        shipPiece.containsShip = true;
+        shipPiece.isHit = false;
+        shipPiece.shipType = `${shipLength}`;
+        shipPiece.sunkStatus = false;
+
+        shipPiece.hitShip = function() {
+            if (this.containsShip === false || this.sinkStatus === true) return;
+            this.isHit = true;
+        };
+    })
+    return ship;
+}
+
+function Gameboard() {
+    let gameboardArr = [
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ],
+    ]
+    gameboardArr.forEach(gameBoardRow => {
+        gameBoardRow.forEach(gameBoardItem => { gameBoardItem.containsShip = false, gameBoardItem.isHit = false })
+    })
+
+    function placeShip(xCoord, yCoord, shipLength) {
+        if (xCoord >= 10 || xCoord < 0 || yCoord < 0 || shipLength < 0) {
+            return false;
+        } else {
+            if (yCoord + shipLength > 10) {
+                return false;
+            } else {
+                let ship = ShipFactory(shipLength)
+                gameboardArr[xCoord].splice(yCoord, shipLength, ...ship)
+                return true;
+            }
+        }
+    }
+
+    function receiveAttack(xCoord, yCoord) {
+        if (gameboardArr[xCoord][yCoord].containsShip === true) {
+            gameboardArr[xCoord][yCoord].hitShip();
+        } else {
+            gameboardArr[xCoord][yCoord].isHit = true;
+        }
+        let currentShipType = gameboardArr[xCoord][yCoord].shipType;
+        updateShipSunkStatus(getShipOfType(currentShipType));
+    }
+
+    function updateShipSunkStatus(shipInfo) {
+        let shipHitCounter = 0;
+        let shipSunkStatus = false;
+        for (let i = 0; i < shipInfo[1].length; i++) {
+            if (gameboardArr[shipInfo[0]][shipInfo[1][i]].isHit === true) {
+                shipHitCounter++;
+            }
+        }
+        if (shipHitCounter === shipInfo[1].length) {
+            shipSunkStatus = true;
+            for (let i = 0; i < shipInfo[1].length; i++) {
+                gameboardArr[shipInfo[0]][shipInfo[1][i]].sunkStatus = true;
+            }
+
+        }
+        console.log(shipHitCounter, shipSunkStatus)
+    }
+
+    function getShipOfType(shipType) {
+        let boardRowIndex;
+        let shipPieceIndexes = '';
+        for (boardRow of gameboardArr) {
+            boardRow.forEach(boardPiece => {
+                if (boardPiece.shipType === shipType) {
+                    boardRowIndex = gameboardArr.indexOf(boardRow);
+                    shipPieceIndexes += boardRow.indexOf(boardPiece);
+                }
+            })
+        }
+        let shipInfo = [boardRowIndex, shipPieceIndexes];
+        return (shipInfo);
+    }
+    return { placeShip, gameboardArr, receiveAttack, getShipOfType };
+}
+
+
+
+module.exports = { Gameboard };
---
+++ src/index.test.js
@@ -0,0 +1,36 @@
+const { Gameboard } = require('./index')
+let testBoard = Gameboard();
+
+
+test('place ship on gameboard', () => {
+    expect(testBoard.placeShip(0, 9, 2)).toBe(false);
+    expect(testBoard.placeShip(0, 9, 1)).toBe(true);
+    expect(testBoard.placeShip(9, 9, 2)).toBe(false);
+    expect(testBoard.placeShip(9, 8, 2)).toBe(true);
+    expect(testBoard.placeShip(-1, 9, -1)).toBe(false);
+    expect(testBoard.placeShip(1, -9, -1)).toBe(false);
+    expect(testBoard.placeShip(1, 9, -1)).toBe(false);
+})
+
+test('hit ship', () => {
+    testBoard.placeShip(0, 0, 2);
+    testBoard.gameboardArr[0][0].hitShip();
+    expect(testBoard.gameboardArr[0][0].isHit).toBe(true)
+})
+
+test('destroy ship', () => {
+    testBoard.placeShip(0, 0, 3);
+    testBoard.receiveAttack(0, 0);
+    testBoard.receiveAttack(0, 1);
+    testBoard.receiveAttack(0, 2);
+    expect(testBoard.gameboardArr[0][0].isHit).toBe(true)
+    expect(testBoard.gameboardArr[0][1].isHit).toBe(true)
+    expect(testBoard.gameboardArr[0][2].isHit).toBe(true)
+    expect(testBoard.gameboardArr[0][0].sunkStatus).toBe(true)
+})
+
+test('hit gameboard', () => {;
+    testBoard.receiveAttack(4, 6);
+    expect(testBoard.gameboardArr[4][6].isHit).toBe(true);
+
+})
