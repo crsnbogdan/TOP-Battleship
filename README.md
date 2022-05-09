636d4fbb588082a42414a09989ce8be09b9a7519
crsnbogdan <crsnbogdan@gmail.com>
2022-05-10 00:37:49 +0300

    added listeners that display when an enemy ship is hit, and when the enemy hit one of the player's board pieces or ship pieces

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 ggitTITLE: Battleship
 
-VERSION NUMBER: 1.1
+VERSION NUMBER: 1.2
 
 DESCRIPTION: This project is focused on using TDD to create a battleship game. The goal is to create a working game without using any visual methods ( such as using console.log() or DOM methods ), and only once the game is working to create the UI
 
 PROJECT STATUS: In Progress
 
 TO DO:
-- show when player board pieces are hit, and when they are ship pieces
 - add special CSS styling for both hit board pieces, and ship pieces
-- implement a way of displaying when the game is over
+- create a module that is shown when the game is over that displays the winner
 - create a module used to get the player's name and initializes the game when submitted, so it can be used in the game
 - update tests
 - create mocks for tests
-- create UI
+- build UI
 
 TECHNOLOGIES USED: HTML, CSS, JS, Webpack, NPM, Jest
 
--- src/Player.js
+++ src/Player.js
@@ -1,83 +1,91 @@
 import { Gameboard } from "./Gameboard.js";
 
 function Player() {
-    const playerName = "ben"; /*prompt('what should we call you?')*/
-    let playerBoard = Gameboard();
-    let compBoard = Gameboard();
-    placeComputerShips();
-    let playerTurn = true;
-    let gameOver = false;
-    let winner = "";
+  const playerName = "ben"; /*prompt('what should we call you?')*/
+  let playerBoard = Gameboard();
+  let compBoard = Gameboard();
+  placeComputerShips();
+  let playerTurn = true;
+  let gameOver = false;
+  let winner = "";
 
-    function checkIfGameEnded() {
-        if (gameOver) return;
-        if (compBoard.reportShipsDestroyedStatus()) {
-            winner = "player";
-            gameOver = true;
-            return;
-        }
-        if (playerBoard.reportShipsDestroyedStatus()) {
-            winner = "computer";
-            gameOver = true;
-            return;
-        }
+  function checkIfGameEnded() {
+    if (gameOver) return;
+    if (compBoard.reportShipsDestroyedStatus()) {
+      winner = "player";
+      gameOver = true;
+      return;
     }
+    if (playerBoard.reportShipsDestroyedStatus()) {
+      winner = "computer";
+      gameOver = true;
+      return;
+    }
+  }
 
-    function attackComputerBoard(xCoord, yCoord) {
-        if (gameOver) return;
-        compBoard.receiveAttack(xCoord, yCoord);
-        switchTurn();
-        playComputerTurn(playerBoard);
-        checkIfGameEnded();
-    }
+  function attackComputerBoard(xCoord, yCoord) {
+    if (gameOver) return;
+    compBoard.receiveAttack(xCoord, yCoord);
+    switchTurn();
+    let cachedCoords = playComputerTurn(playerBoard);
 
-    function switchTurn() {
-        if (playerTurn) {
-            playerTurn = false;
-        } else if (!playerTurn) {
-            playerTurn = true;
-        }
-    }
+    checkIfGameEnded();
+    return cachedCoords;
+  }
 
-    function placeComputerShips() {
-        generateComputerShip(5);
-        generateComputerShip(4);
-        generateComputerShip(3);
-        generateComputerShip(2);
+  function switchTurn() {
+    if (playerTurn) {
+      playerTurn = false;
+    } else if (!playerTurn) {
+      playerTurn = true;
     }
+  }
 
-    function generateTwoRandomCoords() {
-        let x = Number((Math.random() * 10).toFixed(0));
-        let y = Number((Math.random() * 10).toFixed(0));
-        if (x + y > 9) {
-            return generateTwoRandomCoords();
-        } else {
-            return [x, y];
-        }
-    }
+  function placeComputerShips() {
+    setTimeout(generateComputerShip(5), 100);
+    setTimeout(generateComputerShip(4), 200);
+    setTimeout(generateComputerShip(3), 300);
+    setTimeout(generateComputerShip(2), 400);
+  }
 
-    function generateComputerShip(shipLength) {
-        let [x, y] = generateTwoRandomCoords();
-        while (y + shipLength > 9) {
-            y = Number((Math.random() * 10).toFixed(0));
-        }
-        while (compBoard.gameboardArr[x][shipLength].containsShip) {
-            x = Number((Math.random() * 10).toFixed(0));
-        }
-        compBoard.placeShip(x, y, shipLength);
+  function generateTwoRandomCoords(isForAttack) {
+    let x = Math.floor(Number(Math.random() * 10));
+    let y = Math.floor(Number(Math.random() * 10));
+    if (isForAttack) {
+      return [x, y];
+    } else {
+      if (x + y > 9) {
+        return generateTwoRandomCoords();
+      } else {
+        return [x, y];
+      }
     }
+  }
 
-    function playComputerTurn(playerBoard) {
-        if (gameOver) return;
-        let [x, y] = generateTwoRandomCoords();
-        while (playerBoard.gameboardArr[x][y].isHit) {
-            [x, y] = generateTwoRandomCoords();
-        }
-        playerBoard.receiveAttack(x, y);
-        switchTurn();
-        checkIfGameEnded();
+  function generateComputerShip(shipLength) {
+    let [x, y] = generateTwoRandomCoords();
+    while (y + shipLength > 9) {
+      y = Number((Math.random() * 10).toFixed(0));
     }
-    return { attackComputerBoard, gameOver, playerBoard, compBoard };
+    while (compBoard.gameboardArr[x][shipLength].containsShip) {
+      x = Number((Math.random() * 10).toFixed(0));
+      y = Number((Math.random() * 10).toFixed(0));
+    }
+    compBoard.placeShip(x, y, shipLength);
+  }
+
+  function playComputerTurn(playerBoard) {
+    if (gameOver) return;
+    let [x, y] = generateTwoRandomCoords(true);
+    if (playerBoard.gameboardArr[x][y].isHit) {
+      [x, y] = generateTwoRandomCoords(true);
+    }
+    playerBoard.receiveAttack(x, y);
+    switchTurn();
+    checkIfGameEnded();
+    return [x, y];
+  }
+  return { attackComputerBoard, gameOver, playerBoard, compBoard };
 }
 
 export { Player };
