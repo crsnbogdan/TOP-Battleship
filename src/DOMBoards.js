ffc43e227f0de3bf472a49d5958dd02de21e44a3
crsnbogdan <crsnbogdan@gmail.com>
2022-05-09 22:30:43 +0300

    test commit

--- src/DOMBoards.js
+++ src/DOMBoards.js
@@ -1,118 +1,118 @@
 import {
-  updateGamePrompt,
-  getShipType,
-  clearDOMElement,
+    updateGamePrompt,
+    getShipType,
+    clearDOMElement,
 } from "./DOMMethods.js";
 
 function renderGame(playerBoard, compBoard, player) {
-  let gameboard = playerBoard;
+    let gameboard = playerBoard;
 
-  function renderSingleBoardView(gameboard) {
-    let mainViewSection = document.querySelector(".mainviewSection");
-    let gameboardArr = gameboard.gameboardArr;
-    let shipLength = 5;
-    updateGamePrompt(`Please place your ${getShipType(shipLength)}`);
-    let playerBoardContainer = document.createElement("div");
-    playerBoardContainer.classList.add("playerboard-container");
-    mainViewSection.appendChild(playerBoardContainer);
-    let playerBoard = document.createElement("div");
-    playerBoard.classList.add("playerboard");
-    playerBoardContainer.appendChild(playerBoard);
+    function renderSingleBoardView(gameboard) {
+        let mainViewSection = document.querySelector(".mainviewSection");
+        let gameboardArr = gameboard.gameboardArr;
+        let shipLength = 5;
+        updateGamePrompt(`Please place your ${getShipType(shipLength)}`);
+        let playerBoardContainer = document.createElement("div");
+        playerBoardContainer.classList.add("playerboard-container");
+        mainViewSection.appendChild(playerBoardContainer);
+        let playerBoard = document.createElement("div");
+        playerBoard.classList.add("playerboard");
+        playerBoardContainer.appendChild(playerBoard);
 
-    gameboardArr.forEach((gameboardRow) => {
-      let xCoord = gameboardArr.indexOf(gameboardRow);
-      gameboardRow.forEach((rowItem) => {
-        let yCoord = gameboardRow.indexOf(rowItem);
-        const boardItem = document.createElement("div");
-        boardItem.classList.add("boardItem");
-        playerBoard.appendChild(boardItem);
+        gameboardArr.forEach((gameboardRow) => {
+            let xCoord = gameboardArr.indexOf(gameboardRow);
+            gameboardRow.forEach((rowItem) => {
+                let yCoord = gameboardRow.indexOf(rowItem);
+                const boardItem = document.createElement("div");
+                boardItem.classList.add("boardItem");
+                playerBoard.appendChild(boardItem);
 
-        boardItem.addEventListener("click", () => {
-          if (checkAdjacentBoardPieces(shipLength, boardItem)) return;
-          if (shipLength < 1) return;
-          if (gameboard.placeShip(xCoord, yCoord, shipLength) === false) return;
+                boardItem.addEventListener("click", () => {
+                    if (checkAdjacentBoardPieces(shipLength, boardItem)) return;
+                    if (shipLength < 1) return;
+                    if (gameboard.placeShip(xCoord, yCoord, shipLength) === false) return;
 
-          gameboard.placeShip(xCoord, yCoord, shipLength);
-          let currentBoardItem = boardItem;
-          if (!boardItem.previousElementSibling == null) {
-            currentBoardItem = boardItem;
-          }
-          for (let i = 0; i < shipLength; i++) {
-            currentBoardItem.classList.add("containsShip");
-            currentBoardItem = currentBoardItem.nextElementSibling;
-          }
+                    gameboard.placeShip(xCoord, yCoord, shipLength);
+                    let currentBoardItem = boardItem;
+                    if (!boardItem.previousElementSibling == null) {
+                        currentBoardItem = boardItem;
+                    }
+                    for (let i = 0; i < shipLength; i++) {
+                        currentBoardItem.classList.add("containsShip");
+                        currentBoardItem = currentBoardItem.nextElementSibling;
+                    }
 
-          shipLength--;
+                    shipLength--;
 
-          if (shipLength > 1) {
-            updateGamePrompt(`Please place your ${getShipType(shipLength)}`);
-          } else if (shipLength <= 1) {
-            updateGamePrompt("");
-            renderDualBoardView(gameboard, compBoard, mainViewSection);
-          }
+                    if (shipLength > 1) {
+                        updateGamePrompt(`Please place your ${getShipType(shipLength)}`);
+                    } else if (shipLength <= 1) {
+                        updateGamePrompt("");
+                        renderDualBoardView(gameboard, compBoard, mainViewSection);
+                    }
 
-          function checkAdjacentBoardPieces(shipLength, currentBoardItem) {
-            let breakFunction = false;
-            if (breakFunction === true) return;
-            let boardItem = currentBoardItem;
-            for (let i = 0; i < shipLength; i++) {
-              if (breakFunction) return;
-              if (boardItem.classList.contains("containsShip")) {
-                return (breakFunction = true);
-              } else {
-                boardItem = boardItem.nextElementSibling;
-              }
-            }
-            return breakFunction;
-          }
-        });
-      });
-    });
-  }
-
-  function renderDualBoardView(playerBoard, compBoard, containerElement) {
-    clearDOMElement(containerElement);
-    let mainViewSection = document.querySelector(".mainviewSection");
-
-    function renderBoard(board, containerElement, displayShips) {
-      let boardContainer = document.createElement("div");
-      boardContainer.classList.add("boardContainer");
-      containerElement.appendChild(boardContainer);
-
-      let gameboardArr = board.gameboardArr;
-
-      let DOMBoard = document.createElement("div");
-      boardContainer.appendChild(DOMBoard);
-      DOMBoard.classList.add("gameboard");
-      gameboardArr.forEach((gameboardRow) => {
-        gameboardRow.forEach((rowItem) => {
-          const boardItem = document.createElement("div");
-          boardItem.classList.add("boardItem");
-          DOMBoard.appendChild(boardItem);
-          if (displayShips) {
-            if (rowItem.containsShip) {
-              boardItem.classList.add("containsShip");
-            }
-          }
-          if (!displayShips) {
-            boardItem.addEventListener("click", () => {
-              let xCoord = gameboardArr.indexOf(gameboardRow);
-              let yCoord = gameboardRow.indexOf(rowItem);
-
-              player.attackComputerBoard(xCoord, yCoord);
-
-              boardItem.style.backgroundColor = "red";
-              if (rowItem.containsShip)
-                boardItem.style.backgroundColor = "orange";
+                    function checkAdjacentBoardPieces(shipLength, currentBoardItem) {
+                        let breakFunction = false;
+                        if (breakFunction === true) return;
+                        let boardItem = currentBoardItem;
+                        for (let i = 0; i < shipLength; i++) {
+                            if (breakFunction) return;
+                            if (boardItem.classList.contains("containsShip")) {
+                                return (breakFunction = true);
+                            } else {
+                                boardItem = boardItem.nextElementSibling;
+                            }
+                        }
+                        return breakFunction;
+                    }
+                });
             });
-          }
         });
-      });
     }
-    renderBoard(playerBoard, mainViewSection, true);
-    renderBoard(compBoard, mainViewSection, false);
-  }
-  renderSingleBoardView(gameboard);
+
+    function renderDualBoardView(playerBoard, compBoard, containerElement) {
+        clearDOMElement(containerElement);
+        let mainViewSection = document.querySelector(".mainviewSection");
+
+        function renderBoard(board, containerElement, displayShips) {
+            let boardContainer = document.createElement("div");
+            boardContainer.classList.add("boardContainer");
+            containerElement.appendChild(boardContainer);
+
+            let gameboardArr = board.gameboardArr;
+
+            let DOMBoard = document.createElement("div");
+            boardContainer.appendChild(DOMBoard);
+            DOMBoard.classList.add("gameboard");
+            gameboardArr.forEach((gameboardRow) => {
+                gameboardRow.forEach((rowItem) => {
+                    const boardItem = document.createElement("div");
+                    boardItem.classList.add("boardItem");
+                    DOMBoard.appendChild(boardItem);
+                    if (displayShips) {
+                        if (rowItem.containsShip) {
+                            boardItem.classList.add("containsShip");
+                        }
+                    }
+                    if (!displayShips) {
+                        boardItem.addEventListener("click", () => {
+                            let xCoord = gameboardArr.indexOf(gameboardRow);
+                            let yCoord = gameboardRow.indexOf(rowItem);
+
+                            player.attackComputerBoard(xCoord, yCoord);
+
+                            boardItem.style.backgroundColor = "red";
+                            if (rowItem.containsShip)
+                                boardItem.style.backgroundColor = "orange";
+                        });
+                    }
+                });
+            });
+        }
+        renderBoard(playerBoard, mainViewSection, true);
+        renderBoard(compBoard, mainViewSection, false);
+    }
+    renderSingleBoardView(gameboard);
 }
 
 export { renderGame };
--- src/Player.js
+++ src/Player.js
@@ -1,83 +1,83 @@
 import { Gameboard } from "./Gameboard.js";
 
 function Player() {
-  const playerName = "ben"; /*prompt('what should we call you?')*/
-  let playerBoard = Gameboard();
-  let compBoard = Gameboard();
-  placeComputerShips();
-  let playerTurn = true;
-  let gameOver = false;
-  let winner = "";
+    const playerName = "ben"; /*prompt('what should we call you?')*/
+    let playerBoard = Gameboard();
+    let compBoard = Gameboard();
+    placeComputerShips();
+    let playerTurn = true;
+    let gameOver = false;
+    let winner = "";
 
-  function checkIfGameEnded() {
-    if (gameOver) return;
-    if (compBoard.reportShipsDestroyedStatus()) {
-      winner = "player";
-      gameOver = true;
-      return;
+    function checkIfGameEnded() {
+        if (gameOver) return;
+        if (compBoard.reportShipsDestroyedStatus()) {
+            winner = "player";
+            gameOver = true;
+            return;
+        }
+        if (playerBoard.reportShipsDestroyedStatus()) {
+            winner = "computer";
+            gameOver = true;
+            return;
+        }
     }
-    if (playerBoard.reportShipsDestroyedStatus()) {
-      winner = "computer";
-      gameOver = true;
-      return;
-    }
-  }
 
-  function attackComputerBoard(xCoord, yCoord) {
-    if (gameOver) return;
-    compBoard.receiveAttack(xCoord, yCoord);
-    switchTurn();
-    playComputerTurn(playerBoard);
-    checkIfGameEnded();
-  }
-
-  function switchTurn() {
-    if (playerTurn) {
-      playerTurn = false;
-    } else if (!playerTurn) {
-      playerTurn = true;
+    function attackComputerBoard(xCoord, yCoord) {
+        if (gameOver) return;
+        compBoard.receiveAttack(xCoord, yCoord);
+        switchTurn();
+        playComputerTurn(playerBoard);
+        checkIfGameEnded();
     }
-  }
 
-  function placeComputerShips() {
-    generateComputerShip(5);
-    generateComputerShip(4);
-    generateComputerShip(3);
-    generateComputerShip(2);
-  }
+    function switchTurn() {
+        if (playerTurn) {
+            playerTurn = false;
+        } else if (!playerTurn) {
+            playerTurn = true;
+        }
+    }
 
-  function generateTwoRandomCoords() {
-    let x = Number((Math.random() * 10).toFixed(0));
-    let y = Number((Math.random() * 10).toFixed(0));
-    if (x + y > 9) {
-      return generateTwoRandomCoords();
-    } else {
-      return [x, y];
+    function placeComputerShips() {
+        generateComputerShip(5);
+        generateComputerShip(4);
+        generateComputerShip(3);
+        generateComputerShip(2);
     }
-  }
 
-  function generateComputerShip(shipLength) {
-    let [x, y] = generateTwoRandomCoords();
-    while (y + shipLength > 9) {
-      y = Number((Math.random() * 10).toFixed(0));
+    function generateTwoRandomCoords() {
+        let x = Number((Math.random() * 10).toFixed(0));
+        let y = Number((Math.random() * 10).toFixed(0));
+        if (x + y > 9) {
+            return generateTwoRandomCoords();
+        } else {
+            return [x, y];
+        }
     }
-    while (compBoard.gameboardArr[x][shipLength].containsShip) {
-      x = Number((Math.random() * 10).toFixed(0));
-    }
-    compBoard.placeShip(x, y, shipLength);
-  }
 
-  function playComputerTurn(playerBoard) {
-    if (gameOver) return;
-    let [x, y] = generateTwoRandomCoords();
-    while (playerBoard.gameboardArr[x][y].isHit) {
-      [x, y] = generateTwoRandomCoords();
+    function generateComputerShip(shipLength) {
+        let [x, y] = generateTwoRandomCoords();
+        while (y + shipLength > 9) {
+            y = Number((Math.random() * 10).toFixed(0));
+        }
+        while (compBoard.gameboardArr[x][shipLength].containsShip) {
+            x = Number((Math.random() * 10).toFixed(0));
+        }
+        compBoard.placeShip(x, y, shipLength);
     }
-    playerBoard.receiveAttack(x, y);
-    switchTurn();
-    checkIfGameEnded();
-  }
-  return { attackComputerBoard, gameOver, playerBoard, compBoard };
+
+    function playComputerTurn(playerBoard) {
+        if (gameOver) return;
+        let [x, y] = generateTwoRandomCoords();
+        while (playerBoard.gameboardArr[x][y].isHit) {
+            [x, y] = generateTwoRandomCoords();
+        }
+        playerBoard.receiveAttack(x, y);
+        switchTurn();
+        checkIfGameEnded();
+    }
+    return { attackComputerBoard, gameOver, playerBoard, compBoard };
 }
 
 export { Player };
