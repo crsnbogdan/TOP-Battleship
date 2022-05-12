0e94a3ff54a83402c87c63cd99e5db7a38647232
crsnbogdan <crsnbogdan@gmail.com>
2022-05-13 02:23:37 +0300

    add logo as import to be displayed in dist

--- src/modules/InitializeGame.js
+++ src/modules/InitializeGame.js
@@ -1,26 +1,29 @@
 import { clearDOMElement } from "./HelperFunctions.js";
 import { updateGamePrompt } from "./HelperFunctions.js";
+import logo from "../assets/logo.png"
 let playerName = "";
 
 function renderInitialViewContent() {
-  let boardviewSection = document.querySelector(".boardviewSection");
-  clearDOMElement(boardviewSection);
-  updateGamePrompt("Please input your name", document);
-  let form = document.createElement("form");
-  boardviewSection.appendChild(form);
-  let formInput = document.createElement("input");
-  form.appendChild(formInput);
-  let formSubmit = document.createElement("input");
-  formSubmit.setAttribute("id", "submit");
-  formSubmit.setAttribute("type", "submit");
-  formSubmit.setAttribute("value", "START");
-  form.appendChild(formSubmit);
-  form.addEventListener("submit", (e) => {
-    if (formInput.value === "") return;
-    e.preventDefault();
-    let cachedPlayerName = formInput.value;
-    playerName = cachedPlayerName;
-  });
+    let logoImg = document.querySelector('img');
+    logoImg.setAttribute('src', logo)
+    let boardviewSection = document.querySelector(".boardviewSection");
+    clearDOMElement(boardviewSection);
+    updateGamePrompt("Please input your name", document);
+    let form = document.createElement("form");
+    boardviewSection.appendChild(form);
+    let formInput = document.createElement("input");
+    form.appendChild(formInput);
+    let formSubmit = document.createElement("input");
+    formSubmit.setAttribute("id", "submit");
+    formSubmit.setAttribute("type", "submit");
+    formSubmit.setAttribute("value", "START");
+    form.appendChild(formSubmit);
+    form.addEventListener("submit", (e) => {
+        if (formInput.value === "") return;
+        e.preventDefault();
+        let cachedPlayerName = formInput.value;
+        playerName = cachedPlayerName;
+    });
 }
 
 export { renderInitialViewContent, playerName };
